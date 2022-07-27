e65ec6529002a522261fc2eea4dc12c7d00b16f9
ssunny-mengg <khemmaporn.n@ku.th>
2022-07-27 14:25:37 +0700

    lab05

--- src/main/java/th/ac/ku/menu/service/MenuService.java
+++ src/main/java/th/ac/ku/menu/service/MenuService.java
@@ -6,6 +6,7 @@ import th.ac.ku.menu.model.Menu;
 import th.ac.ku.menu.repository.MenuRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MenuService {
@@ -21,5 +22,35 @@ public class MenuService {
         Menu record = menuRepository.save(menu);
         return record;
     }
+
+    public Menu getMenuById(UUID id) {
+        return menuRepository.findById(id).get();
+    }
+
+    public Menu update(Menu requestBody) {
+        UUID id = requestBody.getId();
+        Menu record = menuRepository.findById(id).get();
+        record.setName(requestBody.getName());
+        record.setPrice(requestBody.getPrice());
+        record.setCategory(requestBody.getCategory());
+
+        record = menuRepository.save(record);
+        return record;
+    }
+
+    public Menu delete(UUID id) {
+        Menu record = menuRepository.findById(id).get();
+        menuRepository.deleteById(id);
+        return record;
+    }
+
+    public Menu getMenuByName(String name) {
+        return menuRepository.findByName(name);
+    }
+
+    public List<Menu> getMenuByCategory(String category) {
+        return menuRepository.findByCategory(category);
+    }
+
 
 }
